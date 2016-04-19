85ccc346be515b9624c2f34fd49f35bb1b78542c
Christoffer Schack Steckhahn Jepsen <chri56a4@edu.easj.dk>
2016-04-19 11:01:05 +0200

    WebHandler created, and httpclient created... setting the handler op for getting the data from the database

---
+++ Win10 project/WebHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Win10_project
+{
+    class WebHandler
+    {
+        public static void GetDataFromDB()
+        {
+            const string serverUrl = "http://localhost:9510";
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.UseDefaultCredentials = true;
+        }
+    }
+}
