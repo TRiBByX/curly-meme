6ad4658aa601c0d298d8cb2532053d7e25eee96a
Christoffer Schack Steckhahn Jepsen <chri56a4@edu.easj.dk>
2016-04-19 12:00:54 +0200

    added booking, guest, hotel and room classes for list use...
    a test where a button should make a popup has been implementet in mainpage.xaml.cs

--- Win10 project/WebHandler.cs
+++ Win10 project/WebHandler.cs
@@ -6,6 +6,8 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Popups;
+using Win10WebService;
 
 namespace Win10_project
 {
@@ -16,6 +18,29 @@ namespace Win10_project
             const string serverUrl = "http://localhost:9510";
             HttpClientHandler handler = new HttpClientHandler();
             handler.UseDefaultCredentials = true;
+
+            using (var client = new HttpClient(handler))
+            {
+                client.BaseAddress = new Uri(serverUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                try
+                {
+                    String guestURL = "api/guests";
+                    HttpResponseMessage guestResponseMessage = client.GetAsync(guestURL).Result;
+                    if (guestResponseMessage.IsSuccessStatusCode)
+                    {
+                        var guestlist =
+                            guestResponseMessage.Content.ReadAsAsync<List<Guest>>().Result;
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    new MessageDialog("An error occurred: " + e.Message);
+                }
+            }
         }
     }
 }
